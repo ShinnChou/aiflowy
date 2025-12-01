beb0c9d785b14b7deb1795a4a3fba8bb6885707e
18586361686 <1207669650@qq.com>
2025-12-01 17:52:05 +0800

    feat: apiKey接口及前端

--- aiflowy-ui-admin/app/src/views/config/SysApiKey.vue
+++ aiflowy-ui-admin/app/src/views/config/SysApiKey.vue
@@ -1,42 +1,56 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { markRaw, ref } from 'vue';
 
-import { ElButton } from 'element-plus';
+import { $t } from '@aiflowy/locales';
 
-import PluginSelectModal from '#/components/pluginSelectModal/PluginSelectModal.vue';
+import { Plus } from '@element-plus/icons-vue';
+import { ElDialog } from 'element-plus';
+
+import SysApiKeyList from '#/views/config/apikey/SysApiKeyList.vue';
+import SysApiKeyResourcePermissionList from '#/views/config/apikey/SysApiKeyResourcePermissionList.vue';
 
 const dataSelectRef = ref();
 const handleOpen = () => {
-  dataSelectRef.value.openDialog(['273931608955039744', '280111871524397056']);
+  dialogVisible.value = true;
 };
 const handleGetData = (ids: string[]) => {
   console.log('dataSelectRef.value.getData()');
   console.log(ids);
 };
+const headerButtons = ref([
+  {
+    key: 'add',
+    text: $t('sysApiKeyResourcePermission.addPermission'),
+    icon: markRaw(Plus),
+    type: 'primary',
+    data: { action: 'addPermission' },
+  },
+]);
+const dialogVisible = ref(false);
+const handleButtonClick = (button) => {
+  if (button.data.action === 'addPermission') {
+    handleOpen();
+  }
+};
 </script>
 <template>
   <div class="sys-apikey-container">
-    <div>
-      <ElButton type="primary" @click="handleOpen">
-        {{ $t('button.add') }}
-      </ElButton>
-    </div>
-    <div>
-      <ElButton type="primary" @click="handleGetData"> 确认 </ElButton>
-    </div>
-
-    <PluginSelectModal
-      ref="dataSelectRef"
-      page-url="/api/v1/aiPlugin/page"
-      @get-data="handleGetData"
-      width="60%"
-    />
+    <SysApiKeyList />
+    <ElDialog
+      v-model="dialogVisible"
+      draggable
+      :close-on-click-modal="false"
+      align-center
+    >
+      <SysApiKeyResourcePermissionList />
+    </ElDialog>
   </div>
 </template>
 
 <style scoped>
 .sys-apikey-container {
   height: 100%;
-  display: flex;
+  padding: 20px;
+  width: 100%;
 }
 </style>
---
+++ aiflowy-ui-admin/app/src/views/config/apikey/SysApiKeyResourcePermissionList.vue
@@ -0,0 +1,145 @@
+<script setup lang="ts">
+import type { FormInstance } from 'element-plus';
+
+import { ref } from 'vue';
+
+import { Delete, Edit, Plus } from '@element-plus/icons-vue';
+import {
+  ElButton,
+  ElForm,
+  ElFormItem,
+  ElInput,
+  ElMessage,
+  ElMessageBox,
+  ElTable,
+  ElTableColumn,
+} from 'element-plus';
+
+import { api } from '#/api/request';
+import PageData from '#/components/page/PageData.vue';
+import { $t } from '#/locales';
+
+import SysApiKeyResourcePermissionModal from './SysApiKeyResourcePermissionModal.vue';
+
+const formRef = ref<FormInstance>();
+const pageDataRef = ref();
+const saveDialog = ref();
+const formInline = ref({
+  id: '',
+});
+function search(formEl: FormInstance | undefined) {
+  formEl?.validate((valid) => {
+    if (valid) {
+      pageDataRef.value.setQuery(formInline.value);
+    }
+  });
+}
+function reset(formEl: FormInstance | undefined) {
+  formEl?.resetFields();
+  pageDataRef.value.setQuery({});
+}
+function showDialog(row: any) {
+  saveDialog.value.openDialog({ ...row });
+}
+function remove(row: any) {
+  ElMessageBox.confirm($t('message.deleteAlert'), $t('message.noticeTitle'), {
+    confirmButtonText: $t('message.ok'),
+    cancelButtonText: $t('message.cancel'),
+    type: 'warning',
+    beforeClose: (action, instance, done) => {
+      if (action === 'confirm') {
+        instance.confirmButtonLoading = true;
+        api
+          .post('/api/v1/sysApiKeyResourcePermission/remove', { id: row.id })
+          .then((res) => {
+            instance.confirmButtonLoading = false;
+            if (res.errorCode === 0) {
+              ElMessage.success(res.message);
+              reset(formRef.value);
+              done();
+            }
+          })
+          .catch(() => {
+            instance.confirmButtonLoading = false;
+          });
+      } else {
+        done();
+      }
+    },
+  }).catch(() => {});
+}
+</script>
+
+<template>
+  <div class="page-container">
+    <SysApiKeyResourcePermissionModal ref="saveDialog" @reload="reset" />
+    <ElForm ref="formRef" :inline="true" :model="formInline">
+      <ElFormItem :label="$t('sysApiKeyResourcePermission.title')" prop="title">
+        <ElInput
+          v-model="formInline.title"
+          :placeholder="$t('sysApiKeyResourcePermission.title')"
+        />
+      </ElFormItem>
+      <ElFormItem>
+        <ElButton @click="search(formRef)" type="primary">
+          {{ $t('button.query') }}
+        </ElButton>
+        <ElButton @click="reset(formRef)">
+          {{ $t('button.reset') }}
+        </ElButton>
+      </ElFormItem>
+    </ElForm>
+    <div class="handle-div">
+      <ElButton @click="showDialog({})" type="primary">
+        <ElIcon class="mr-1">
+          <Plus />
+        </ElIcon>
+        {{ $t('button.add') }}
+      </ElButton>
+    </div>
+    <PageData
+      ref="pageDataRef"
+      page-url="/api/v1/sysApiKeyResourcePermission/page"
+      :page-size="10"
+    >
+      <template #default="{ pageList }">
+        <ElTable :data="pageList" border>
+          <ElTableColumn
+            prop="requestInterface"
+            :label="$t('sysApiKeyResourcePermission.requestInterface')"
+          >
+            <template #default="{ row }">
+              {{ row.requestInterface }}
+            </template>
+          </ElTableColumn>
+          <ElTableColumn
+            prop="title"
+            :label="$t('sysApiKeyResourcePermission.title')"
+          >
+            <template #default="{ row }">
+              {{ row.title }}
+            </template>
+          </ElTableColumn>
+          <ElTableColumn :label="$t('common.handle')" width="150">
+            <template #default="{ row }">
+              <ElButton @click="showDialog(row)" link type="primary">
+                <ElIcon class="mr-1">
+                  <Edit />
+                </ElIcon>
+                {{ $t('button.edit') }}
+              </ElButton>
+              <ElButton @click="remove(row)" link type="danger">
+                <ElIcon class="mr-1">
+                  <Delete />
+                </ElIcon>
+                {{ $t('button.delete') }}
+              </ElButton>
+            </template>
+          </ElTableColumn>
+        </ElTable>
+      </template>
+    </PageData>
+  </div>
+</template>
+
+<style scoped></style>
