e63e73a062924686831020e7afe13a220275871b
Henry <henrycoder@qq.com>
2025-12-31 17:24:50 +0800

    feat(preferences): add defaultHomePath

--- aiflowy-ui-admin/app/src/preferences.ts
+++ aiflowy-ui-admin/app/src/preferences.ts
@@ -10,6 +10,7 @@ export const overridesPreferences = defineOverridesPreferences({
   app: {
     name: import.meta.env.VITE_APP_TITLE,
     accessMode: 'mixed',
+    defaultHomePath: '/ai/bots',
   },
   widget: {
     timezone: false,
